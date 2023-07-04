fba05b199f6314636363454d4df899eaf6d45318
trantuxp <tqtu.19it2@vku.udn.vn>
2023-07-04 17:13:53 +0700

    update login v1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\MainController;
 use Illuminate\Support\Facades\Route;
+use \App\Http\Controllers\Admin\User\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,9 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/home', function () {
-    return view('Home');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+// Route::get('/home', function () {
+//     return view('Home');
+// });
+
+Route::get('admin/user/login',[LoginController::class, 'index1']);
+Route::post('admin/user/login/store',[LoginController::class, 'store']);
+
+
+Route::get('admin/main',[MainController::class, 'index'])->name('admin');
